5743dc00044593408ed6aea90400c68759305218
dougc <dougc@localhost.localdomain>
2015-08-03 07:55:14 -0700

    first

---
+++ hw11a/src/main/java/cs246workinggroup/hw11a/App.java
@@ -0,0 +1,13 @@
+package cs246workinggroup.hw11a;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
